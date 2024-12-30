7800b67d51818fc29797d92d0546bef325a93cfd
isavonohlen <isa.von.ohlen@gmail.com>
2024-12-30 20:06:35 +0100

    web5

---
+++ portfolio/sketch.js
@@ -0,0 +1,9 @@
+// Hamburger-Menü Interaktion
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const navMenu = document.getElementById("nav-menu");
+
+    hamburger.addEventListener("click", function () {
+        navMenu.classList.toggle("active"); // Menü ein-/ausblenden
+    });
+});
